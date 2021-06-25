42541c3f8acd0d9bf56ae99dfb69121b44bc62b6
Sophia Yougin <sophiya273@gmail.com>
2021-06-25 21:27:05 +0530

    change pictures

--- src/photos.js
+++ src/photos.js
@@ -1,114 +1,47 @@
-import image1 from './image1.jpeg';
-import image2 from './image2.jpeg';
-import image3 from './image3.jpeg';
-import image4 from './image4.jpeg';
-import image5 from './image5.jpeg';
-import image6 from './image6.jpeg';
-import image7 from './image7.jpeg';
-import image8 from './image8.jpeg';
-import image9 from './image9.jpeg';
-import image10 from './image10.jpeg';
-import image11 from './image11.jpeg';
-import image12 from './image12.jpeg';
-import image13 from './image13.jpeg';
-import image14 from './image14.jpg';
-import image16 from './image16.jpeg';
-import image17 from './image17.jpeg';
-import image18 from './image18.jpeg';
-import image19 from './image19.jpeg';
-
 export const photos = [
-    {
-      src: image1,
-      width: 2,
-      height: 2
-    },
-    {
-      src:image2,
-      width: 1,
-      height: 1.2 
-    },
-    {
-      src: image3,
-      width: 3.1,
-      height: 4
-    },
-    {
-      src: image4,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image5,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image6,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image7,
-      width: 4,
-      height: 4
-    },
-    {
-      src: image8,
-      width: 3,
-      height: 3
-    },
-    {
-      src: image9,
-      width: 3,
-      height: 3
-    },
-    {
-      src: image10,
-      width: 1.5,
-      height: 2
-    },
-    {
-      src:image11,
-      width: 1,
-      height: 1.2 
-    },
-    {
-      src: image12,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image13,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image14,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image16,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image17,
-      width: 3,
-      height: 4
-    },
-    {
-      src: image18,
-      width: 3,
-      height: 3
-    },
-    {
-      src: image19,
-      width: 3,
-      height: 3
-    }
-    
-    
-  ];
-  
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  }
+];
