95f625e854624d62bbd1c195ecc01f630ea3679c
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-08-20 13:31:14 -0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="About">
-        <img src={profilePic}/>
+        <img src={profilePic} alt="" />
         <p>
           {" "}
           I am a Chandigarh based Malayalee Photographer.I love everything{" "}
