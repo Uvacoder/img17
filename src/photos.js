36e1edb2c85924ad84bcb912f0450b40f31d43ee
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-09-02 04:45:27 -0400

    Update photos.js

--- src/photos.js
+++ src/photos.js
@@ -20,26 +20,6 @@ export const photos = [
     height: 4
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-    width: 3,
-    height: 4
-  },
-  {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-    width: 4,
-    height: 3
-  },
-  {
     src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
     width: 4,
     height: 3
